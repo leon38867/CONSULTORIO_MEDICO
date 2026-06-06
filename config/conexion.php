eac54ba003bed447698b1be4e2e75494a5038851
gommy chompis <leoncristobalcervantes@gmail.com>
2026-06-05 22:45:44 -0600

    conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -11,6 +11,14 @@ const DB_PASS = 'dGkSwdqkiqDQkqDLquJWZANVYVnZVIjZ';
 const DB_PORT = 11902;
 const DB_CHARSET = 'utf8mb4';
 
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$conn->set_charset(DB_CHARSET);
+
 define('BASE_PATH', dirname(__DIR__));
 define('UPLOAD_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'documents');
 define('BACKUP_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . 'backups');
